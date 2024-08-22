f3bbe8f2da5b6f5d685bbfe1e63be321d342dc2e
Axel <13357039+axel-ga@users.noreply.github.com>
2024-08-22 12:37:01 +0200

    optimized data format

--- ml_logic/preprocessor/dl_preprocessing.py
+++ ml_logic/preprocessor/dl_preprocessing.py
@@ -4,9 +4,35 @@ import os
 
 from params import *
 
+# Helper function
+# https://www.quora.com/What-temperature-is-cold-in-Fahrenheit
+def temp_classification(x):
+  if x >= 75:
+    return 10
+  elif x >= 65:
+    return 9
+  elif x >= 55:
+    return 8
+  elif x >= 45:
+    return 7
+  elif x >= 35:
+    return 6
+  elif x >= 25:
+    return 5
+  elif x >= 15:
+    return 4
+  elif x >= 5:
+    return 3
+  elif x >= -5:
+    return 2
+  else:
+    return 1
+
+# Write to CSV
 def write_to_csv(df, file_name):
     df.to_csv(file_name+'.csv',mode='w+' )
 
+# Resize DF
 def resize_tracks_df(track_df, step):
     track_df = track_df.iloc[::step, :]
     print("===== resized tracks ======")
@@ -14,6 +40,8 @@ def resize_tracks_df(track_df, step):
     write_to_csv(track_df, 'resize')
     return track_df
 
+
+# Load data
 def load_data(INJURY_DF,PLAYLIST_DF,TRACKS_DF):
     injury_path = os.path.join(DIR_PATH,DATA_FILE,INJURY_DF)
     playlist_path = os.path.join(DIR_PATH,DATA_FILE,PLAYLIST_DF)
@@ -23,6 +51,25 @@ def load_data(INJURY_DF,PLAYLIST_DF,TRACKS_DF):
     tracks_df = pd.read_csv(tracks_path)
     return injury_df, playlist_df, tracks_df
 
+# Transform to parquet
+def transform_format(injury_df, playlist_df, tracks_df):
+    injury_df.to_parquet("cleaned_injury.parquet")
+    playlist_df.to_parquet("cleaned_playlist.parquet")
+    tracks_df.to_parquet("cleaned_tracks.parquet")
+
+    injury_df = pd.read_parquet("cleaned_injury.parquet")
+    playlist_df = pd.read_parquet("cleaned_playlist.parquet")
+    tracks_df = pd.read_parquet("cleaned_tracks.parquet")
+    return injury_df, playlist_df, tracks_df
+
+# Optimize datatype
+def optimize_data_type(injury_df, playlist_df, tracks_df):
+    injury_df.select_dtypes(include='object').astype("category")
+    playlist_df.select_dtypes(include='object').astype("category")
+    tracks_df.select_dtypes(include='object').astype("category")
+    return injury_df, playlist_df, tracks_df
+
+# Clean data
 def clean_injury_df(injury_df):
     # ----FILLNA ---- create a playkeyID column with playkey na filled with mode
     # TODO --> MAKE POSSIBLE TO IMPLEMENT OTHER FILL THAN MODE IN ARGUMENT
@@ -59,6 +106,8 @@ def clean_tracks_df(track_df):
     except:
         return track_df
 
+# Engineer data
+# Injury
 def engineering_injury_df(injury_df):
     # Add injury_duration column
     injury_df['injury_duration'] = injury_df[['DM_M1','DM_M7','DM_M28','DM_M42']].sum(axis=1)
@@ -66,7 +115,8 @@ def engineering_injury_df(injury_df):
     print(injury_df.head(1))
     return injury_df
 
-def engineering_playlist_df(playlist_df):
+# Playlist
+def engineering_playlist_df_max_game(playlist_df):
     # Add max_game column : max number of game for each player
     playlist_df['max_game'] = playlist_df['PlayKey'].str.split("-").str[1]
     playlist_df['max_game']=playlist_df['max_game'].astype(int)
@@ -86,6 +136,25 @@ def engineering_playlist_df(playlist_df):
     print(playlist_df.head(1))
     return playlist_df
 
+def engineering_playlist_df_weather(playlist_df, dict):
+    playlist_df['Wet'] = playlist_df['Weather'].map(dict)
+    print("===== engineering_playlist_df WEATHER ======")
+    print(playlist_df.head(1))
+    return playlist_df
+
+def engineering_playlist_df_indor(playlist_df, dict):
+    playlist_df['Indoor'] = playlist_df['StadiumType'].map(dict)
+    print("===== engineering_playlist_df Indoor ======")
+    print(playlist_df.head(1))
+    return playlist_df
+
+def engineering_playlist_df_temp(playlist_df):
+    playlist_df['Temperature_transformed'] = playlist_df['Temperature'].map(temp_classification)
+    print("===== engineering_playlist_df Temperature ======")
+    print(playlist_df.head(1))
+    return playlist_df
+
+# Tracks
 def engineering_tracks_df_split(track_df):
     # Split playkey col for further taks into game Id, player Id, playkey Id
     track_df['PlayerKey'] = track_df['PlayKey'].str.split("-").str[0]
@@ -98,10 +167,10 @@ def engineering_tracks_df_split(track_df):
 def engineering_tracks_df_true_distance(track_df):
     # Compute true distance
     track_df['dist_x'] = (track_df.sort_values(['PlayKey','time']).groupby(['PlayerKey', 'GameKey'])['x']
-                   .transform(lambda x: x.diff()))
+                   .transform(lambda x: round(x.diff(),2)))
     track_df['dist_y'] = (track_df.sort_values(['PlayKey','time']).groupby(['PlayerKey', 'GameKey'])['y']
-                    .transform(lambda x: x.diff()))
-    track_df['true_dist'] = np.sqrt(track_df['dist_x']**2 + track_df['dist_y']**2)
+                    .transform(lambda x: round(x.diff(),2)))
+    track_df['true_dist'] = round(np.sqrt(track_df['dist_x']**2 + track_df['dist_y']**2),2)
     print("===== true distance ======")
     print(track_df.head(1))
     return track_df
@@ -109,9 +178,9 @@ def engineering_tracks_df_true_distance(track_df):
 def engineering_tracks_df_turn(track_df):
     # Turn and turn aggregation
     track_df['turn'] = (track_df.sort_values(['PlayKey','time']).groupby(['PlayerKey', 'GameKey'])['dir']
-                   .transform(lambda x: x.diff()))
+                   .transform(lambda x: round(x.diff(),2)))
     track_df['turn_agg'] = (track_df.sort_values(['PlayKey','time']).groupby(['PlayerKey', 'GameKey'])['turn']
-                   .transform(lambda x: x.rolling(5, min_periods=1).sum()))
+                   .transform(lambda x: round(x.rolling(5, min_periods=1).sum(),2)))
 
     print("===== Turn ======")
     print(track_df.head(1))
@@ -119,14 +188,14 @@ def engineering_tracks_df_turn(track_df):
 
 def engineering_tracks_df_true_speed(track_df):
     # True speed
-    track_df['true_speed'] = track_df['true_dist']/track_df['time']
+    track_df['true_speed'] = round(track_df['true_dist']/track_df['time'],2)
     print("===== speed ======")
     print(track_df.head(1))
     return track_df
 
 def engineering_tracks_df_degree_diff(track_df):
     # degree diff between orientation and direction
-    track_df['dir_o_diff'] = track_df['dir']-track_df['o']
+    track_df['dir_o_diff'] = round(track_df['dir']-track_df['o'],2)
     print("===== degrre diff ======")
     print(track_df.head(1))
     return track_df
@@ -153,8 +222,10 @@ def engineering_tracks_df_violent_turn(track_df):
     print(track_df.head(1))
     return track_df
 
+# Merge the DFs
 def merge_df(injury_df,playlist_df,tracks_df):
-    merged_playlist_injury = playlist_df.merge(how='left', right=injury_df, on='PlayerKey',suffixes=('', '_injury'))
+    merged_playlist_injury = playlist_df.merge(how='left', right=injury_df, left_on='PlayKey',right_on='new_PlayKey',suffixes=('', '_injury'))
     merged_playlist_injury_tracks = tracks_df.merge(how='left', right=merged_playlist_injury, on='PlayKey',suffixes=('_x', '_tracks'))
     print(merged_playlist_injury_tracks.head(1))
+    write_to_csv(merged_playlist_injury_tracks, 'final')
     return merged_playlist_injury_tracks
